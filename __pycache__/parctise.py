07b54fcd23643b6b10ce76ca2f9d5cea0be1262e
Chandresh-spec <chandumoger819@gmail.com>
2025-09-04 22:30:34 +0530

    revision

---
+++ __pycache__/parctise.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+arr=np.array([1,2,3,4])
+
+
+
+print(arr)
+
+
+
+arr2d=np.array([[1,2,3],
+                [2,4,6]])
+
+
+print(arr2d)
+
+
+matrix=np.array([[[1,2,3],
+         [4,4,6]],
+        [ [4,8,9],
+         [8,9,3]]])
+
+
+print(matrix)
+
+
+
+default0=np.zeros((2,6))
+
+print(default0)
+
+
+
+default1=np.ones((3,3))
+print(default1)
+
+
+
+defall=np.full((2,3),10)
+
+print(defall)
+
+
+
+#creating sequence of number in numpy
+
+
+seq=np.arange(2,10)
+
+print(seq)
+
+
+iden=np.identity(5)
+
+print(iden)

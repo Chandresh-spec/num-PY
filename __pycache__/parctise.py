0f3c98c882e6a25fd4de6210479c546c14161a43
Chandresh-spec <chandumoger819@gmail.com>
2025-09-17 20:28:58 +0530

    learned concatentae

--- __pycache__/parctise.py
+++ __pycache__/parctise.py
@@ -183,3 +183,13 @@ import numpy as np
 arr=np.array([[1,2],[4,5],[6,7]])
 newarr=np.append(arr,[[10],[20],[30]],axis=1)
 
+
+
+# concatenation
+
+import numpy as np
+
+arr1=np.array([[1,2],[3,4]])
+arr2=np.array([[2],[4]])
+
+newarr=np.concatenate((arr1,arr2),axis=1)

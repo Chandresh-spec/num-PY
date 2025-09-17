ce40acb6c57ffadd4de9d9c24563c8446d0156ef
Chandresh-spec <chandumoger819@gmail.com>
2025-09-17 19:47:28 +0530

    learned faltten,ravel,shape

--- __pycache__/flatteningArray.py
+++ __pycache__/flatteningArray.py
@@ -10,4 +10,32 @@ print(arr.reshape(-1))
 
 
 
+arr = np.array([[1, 2], [3, 4]])
 
+
+flat=arr.flatten()
+
+print(flat)
+
+print(arr)
+
+
+
+
+arr = np.array([[1, 2, 3],
+                [4, 5, 6]])
+
+
+print(arr.reshape(-1))
+
+
+
+
+arr = np.array([[1, 2], [3, 4]])
+
+
+flat=arr.ravel()
+
+print(flat)
+
+print(arr)

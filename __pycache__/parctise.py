3860a911a2549414d2d7da50aa5e82ef26c9da1f
Chandresh-spec <chandumoger819@gmail.com>
2025-09-17 20:23:09 +0530

    learned append in numpy

--- __pycache__/parctise.py
+++ __pycache__/parctise.py
@@ -174,3 +174,12 @@ arr=np.array([[1,2,3,4],
 newarr=np.insert(arr,0,[10,20,30],axis=1)
 
 print(newarr)
+
+
+
+
+import numpy as np
+
+arr=np.array([[1,2],[4,5],[6,7]])
+newarr=np.append(arr,[[10],[20],[30]],axis=1)
+
